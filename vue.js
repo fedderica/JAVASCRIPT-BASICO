86893ce88a8d96b035a3d36b51c6da923563dd9f
fedderica <fedderica.bartoli@gmail.com>
2023-04-20 21:51:20 -0300

    vue.ejemplos

--- vue.js
+++ vue.js
@@ -13,3 +13,49 @@ new Vue({
 </script>
 
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+
+
+<div id="app">
+  {{ mensaje }}
+</div>
+
+<script>
+new Vue({
+  el: '#app',
+  data: {
+    mensaje: '¡Hola, mundo!'
+  }
+})
+</script>
+
+
+<div id="app">
+  <ul>
+    <li v-for="fruta in frutas">{{ fruta }}</li>
+  </ul>
+</div>
+
+<script>
+new Vue({
+  el: '#app',
+  data: {
+    frutas: ['manzana', 'banana', 'cereza', 'uva']
+  }
+})
+</script>
+
+
+<div id="app">
+  <button v-on:click="saludar">Saludar</button>
+</div>
+
+<script>
+new Vue({
+  el: '#app',
+  methods: {
+    saludar: function () {
+      alert('¡Hola, amigo!')
+    }
+  }
+})
+</script>
