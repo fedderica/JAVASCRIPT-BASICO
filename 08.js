22130734082148346999edce8b70fab11ba0ea34
fedderica <fedderica.bartoli@gmail.com>
2023-05-05 10:47:19 -0300

    09.js. FOR ejemploeach

--- 08.js
+++ 08.js
@@ -3,3 +3,15 @@ const array = [1, 2, 3, 4, 5];
 array.forEach(function(element) {
   console.log(element);
 });
+
+
+const person = {
+    name: 'John',
+    age: 30,
+    hobbies: ['reading', 'running', 'swimming']
+  };
+  
+  person.hobbies.forEach(function(hobby) {
+    console.log(this.name + ' likes ' + hobby);
+  }, person);
+  
